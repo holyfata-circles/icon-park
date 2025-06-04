e8888f11343622ffc747ded230a61f7aa68008ae
heyongqi10 <heyongqi10@jd.com>
2025-06-04 17:26:57 +0800

    feat: use json to ast

--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,3 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(++counter))
-  setCounter(0)
-}
+import Icon from './icon';
+
+export { Icon };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'output': path.resolve(__dirname, 'output'),
+    },
+  },
   build: {
     lib: {
       entry: './lib/main.ts',
