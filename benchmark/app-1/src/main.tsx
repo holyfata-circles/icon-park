a9f7087928c9950ff651d1342a1154c866a89991
heyongqi10 <heyongqi10@jd.com>
2025-06-04 20:38:13 +0800

    feat: init the benchmark

---
+++ benchmark/app-1/src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Aiming } from "@icon-park/react";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<div>
+			<h1>1</h1>
+			<Aiming />
+		</div>
+	</React.StrictMode>
+);
---
+++ benchmark/app-2/src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { IconAiming } from "@holyfata/icon-park";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<div>
+			<h1>2</h1>
+			<IconAiming />
+		</div>
+	</React.StrictMode>
+);
